010eae56a2a664f3bd0b096e2d23ac8c646a1350
Abhinaba Chakraborty <abhinaba.1992@gmail.com>
2017-12-17 22:40:49 +0530

    Update Clustering and Segmentation P11.R

--- R-Data Visualization Assignments/R Training Materials/Clustering and Segmentation P11.R
+++ R-Data Visualization Assignments/R Training Materials/Clustering and Segmentation P11.R
@@ -2,14 +2,17 @@
 # We are using the data set mtcars here.
 
 #Author: Abhinaba Chakraborty
-#last Updated: 12.12.2017
+#last Updated: 17.12.2017
 
 data(mtcars)
 df_cars=mtcars
 
+#Hierarchical clustering
+
+
 # 1. STANDARDISING THE DATASET
 
-#Here we are standardising with median and median absolute deviation instead of (mean/standard deviation)
+#Here we are standardising with median and median absolute deviation instead of (mean and standard deviation)
 library(dplyr)
 
 #Calculating the medians of all the columns
@@ -22,7 +25,7 @@ mads=apply(df_cars,2,mad) # mad=median(abs(xi-median(x)))
 cars.std=data.frame(scale(df_cars,center=medians,scale = mads))
 #In case we would have used mean and standard deviation, we would have written the above eqn., in the following
 #way, as by default mean and standard deviation will be passed if we are not passing any value
-#cars.std=data.frame(scale(df_cars))
+#cars.std=data.frame(scale(df_cars))  
 
 head(cars.std)
 #Now, we found out that columns vs and am are having NAN and INF fields in them, so we may choose to remove those 
@@ -56,7 +59,7 @@ cars.dist
 # 3. PASSING THE DISTANCE MATRIX TO HIERARCHICAL CLUSTERING
 
 cars.hclust=hclust(cars.dist)
-#So we find out that complete linkage is used for clustering our data
+#By Default Complete linkage is used for clustering our data
 
 #In case we wish to use Single or average linkage, we do the following
 #cars.hclust=hclust(cars.dist,method="single") OR cars.hclust=hclust(cars.dist,method="average")
@@ -84,3 +87,57 @@ library(cluster)
 diss=daisy(cars.std) #We are passing the standardised data fr our car data set
 sk=silhouette(groups.2,diss)
 plot(sk)
+
+
+#If we wish to change the cluster to which any records belongs, we can do the following
+cars.std$cluster=groups.2
+cars.std$cluster[1]=2
+View(cars.std)
+
+
+#K-means clustering
+#Here we are randomly creating a data set
+n=100
+g=6
+set.seed(g)
+d = data.frame(
+    x=unlist(lapply(1:g,function(i) rnorm(n/g, runif(1)*i^2))),
+    y=unlist(lapply(1:g,function(i) rnorm(n/g, runif(1)*i^2)))
+)
+plot(d,col="mediumturquoise",pch=16,
+     xlab="Arrival Time Deviations",
+     ylab="Departure Time Deviations",
+     main="Scatter plot: Delays from schedule in Minutes"
+)
+
+
+#Demonstrating K-means
+mydata=d
+
+#Determine the optimal cluster size based on within sum of square
+#within sum of square is the degrees of freedom * variance of the dataset
+wss=(nrow(mydata)-1)*sum(apply(mydata,2,var))
+
+#Now we are doing the same thing as we did above, but with k-means
+#Here the i in the loop denotes the number of clusters and we iterate through multiple combos (2 to 5 clusters)
+#so as to determine the k-means value for 2 to 15 number of clusters (the loop starts at 2 because the minimum 
+#number of clusters can be 2) 
+for (i in 2:15) wss[i]=sum(kmeans(mydata,centers = i)$withinss)
+
+#The above helps us to determine; what is the optimal number of clusters where the wss value is low and after which
+#there will be no change of variance among the values of the cluster
+#So, we are plotting an elbow chart/scree plot to determine the optimal no. of cluster
+plot(1:15,wss,type="b",xlab="Number of clusters",ylab="within groups sum of squares",col="mediumseagreen",pch=12)
+
+ 
+#So from the elbow chart we concluded that we can have 4 number of clusters for our dataset
+fit=kmeans(mydata,4)
+
+#Adding the calculated cluster to the original data set d
+d$cluster=fit$cluster
+
+library(ggplot2)
+ggplot(d,aes(x=cluster))+geom_bar(aes(fill=cluster))
+
+
+#Taking a real time data set to test k-means
